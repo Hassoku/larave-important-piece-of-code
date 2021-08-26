05e4ebedb96c5793a0da0932eb7e5201d21e712b
Hassoku <mhhassan095@gmail.com>
2021-08-26 08:58:16 -0700

    from an to code updated

--- FromToDateFilter.php
+++ FromToDateFilter.php
@@ -22,3 +22,35 @@ function fromAndToDateFilter($from, $to, $query, $column = 'created_at', $strToT
 
     return $query;
 }
+
+//Controller methods
+private function filters($query, $request)
+{
+    $from = $request->get('from', null);
+    $to = $request->get('to', null);
+    $title = $request->get('title', null);
+    $category_id = $request->get('category_id', null);
+    $author_id = $request->get('author_id', null);
+    $status = $request->get('status', null);
+
+    $query = fromAndToDateFilter($from, $to, $query, 'created_at');
+
+
+    if (!empty($title)) {
+        $query->where('title', 'like', '%' . $title . '%');
+    }
+
+    if (!empty($category_id)) {
+        $query->where('category_id', $category_id);
+    }
+
+    if (!empty($author_id)) {
+        $query->where('author_id', $author_id);
+    }
+
+    if (!empty($status)) {
+        $query->where('status', $status);
+    }
+
+    return $query;
+}
