3e5233834e742e58bbd7a817772a1380cea870bd
HaseebHassan <mhhassan095@gmail.com>
2021-11-12 16:56:17 -0800

    routine commit

--- monthlydata.php
+++ monthlydata.php
@@ -2,3 +2,7 @@
 $applications = DB::table('applications')->select(DB::raw('DATE(created_at) as created_at'), DB::raw('COUNT(*) as total'))->
         groupBy(DB::raw('DATE(created_at)'))->
         get();
+
+
+
+    
